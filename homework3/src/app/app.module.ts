e4b5c3d4a751e61acf61db9a55b59d936f41621a
HOME <kirill.tretyakoff@gmail.com>
2021-01-25 10:19:41 +0200

    My 3 Angular Homework

---
+++ homework3/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {CarsComponent} from './components/cars/cars.component';
+import {CarComponent} from './components/car/car.component';
+import {HomeComponent} from './components/home/home.component';
+import {RouterModule} from '@angular/router';
+import {FullCarComponent} from './components/full-car/full-car.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarsComponent,
+    CarComponent,
+    HomeComponent,
+    FullCarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'link/home', component: HomeComponent
+      },
+      {
+        path: 'link/cars', component: CarsComponent, children: [
+          {path: ':id', component: FullCarComponent}
+        ]
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ homework3/src/app/models/Cars.ts
@@ -0,0 +1,8 @@
+export interface Car {
+  id: number;
+  brand: string;
+  model: string;
+  color: string;
+  power: number;
+  hp: number;
+}
